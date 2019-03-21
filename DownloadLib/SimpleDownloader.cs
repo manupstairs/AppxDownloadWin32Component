a2c2d83c172e3d530cd16b49c19b4eb3a52b023b
Zheng_Xing <zheng_xing@dell.com>
2019-03-21 17:47:57 +0800

    refactor SimpleDownloader

--- DownloadLib/SimpleDownloader.cs
+++ DownloadLib/SimpleDownloader.cs
@@ -9,9 +9,7 @@ namespace DownloadLib
 {
     public class SimpleDownloader
     {
-        private int BufferSize => 16 * 1024;
-
-        public async Task<string> DownloadFile(string uriString, string saveFileName, string userName, string password)
+        public async Task<Stream> RequestHttpContentAsync(string uriString, string userName, string password)
         {
             using (HttpClient client = new HttpClient())
             {
@@ -26,20 +24,7 @@ namespace DownloadLib
                 {
                     HttpContent content = response.Content;
                     var contentStream = await content.ReadAsStreamAsync();
-                    var savePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, saveFileName);
-                    using (var fileStream = new FileStream(savePath, FileMode.Create))
-                    {
-                        byte[] buffer = new byte[BufferSize];
-                        int bytesRead;
-                        do
-                        {
-                            bytesRead = contentStream.Read(buffer, 0, BufferSize);
-                            fileStream.Write(buffer, 0, bytesRead);
-                        } while (bytesRead > 0);
-
-                    }
-
-                    return savePath;
+                    return contentStream;
                 }
                 else
                 {
--- WPFClient/MainWindow.xaml.cs
+++ WPFClient/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -38,19 +39,38 @@ namespace WPFClient
                 txtLog.AppendText($"Start downloading.\n");
                 this.progressBar.Visibility = Visibility.Visible;
                 var downloader = new SimpleDownloader();
-                var path = await downloader.DownloadFile(
+                var stream = await downloader.RequestHttpContentAsync(
                     ConfigurationManager.AppSettings["uri"],
-                    "installerFile.msi",
                     ConfigurationManager.AppSettings["username"],
                     ConfigurationManager.AppSettings["password"]);
 
                 this.progressBar.Visibility = Visibility.Collapsed;
                 txtLog.AppendText($"Done.\n");
+
+                var path = SaveFile(stream);
                 txtLog.AppendText($"File path is {path}.\n");
 
-                txtLog.AppendText($"Start process {path}.\n");
                 Process.Start(path);
+                txtLog.AppendText($"Start process {path}.\n");
             }
+        }
+
+        private string SaveFile(Stream stream)
+        {
+            var filePath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "installFile.msi");
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                byte[] buffer = new byte[2048];
+                int bytesRead;
+                do
+                {
+                    bytesRead = stream.Read(buffer, 0, 2048);
+                    fileStream.Write(buffer, 0, bytesRead);
+                } while (bytesRead > 0);
+
+            }
+
+            return filePath;
         }
     }
 }
